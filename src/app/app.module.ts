76db91470cf7aad2f7e72d8245b6bb72a5df55b2
lisaliza6428 <elizavetaivanushenko@gmail.com>
2022-11-06 16:30:31 +0300

    fix app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
-import { MapComponent } from './map/map.component';
-
 import { HttpClientModule } from '@angular/common/http';
-
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { FiltersComponent } from './filters/filters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
